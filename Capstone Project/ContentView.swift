da1f2e78ccc7e371612a37ab24fb19ad8ad287ce
Chase Rodie <89370228+Chase-Rodie@users.noreply.github.com>
2024-09-26 04:27:12 -0700

    Test Commit

--- Capstone Project/ContentView.swift
+++ Capstone Project/ContentView.swift
@@ -7,13 +7,16 @@
 
 import SwiftUI
 
+
+    //NAME: - Chase
+
 struct ContentView: View {
     var body: some View {
         VStack {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("This is a test!")
         }
         .padding()
     }
