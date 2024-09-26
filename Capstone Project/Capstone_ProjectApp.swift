956cc7c68b41c0a10ff8df4f6391642e19552e71
Chase Rodie <89370228+Chase-Rodie@users.noreply.github.com>
2024-09-26 04:24:01 -0700

    Initial Commit

---
+++ Capstone Project/Capstone_ProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  Capstone_ProjectApp.swift
+//  Capstone Project
+//
+//  Created by Chase Rodie on 9/26/24.
+//
+
+import SwiftUI
+
+@main
+struct Capstone_ProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
