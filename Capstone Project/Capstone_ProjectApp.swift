d71ad99fc624b0c2ef9adbf1c0a36e15d2c06da0
Chase Rodie <89370228+Chase-Rodie@users.noreply.github.com>
2024-10-15 13:11:08 -0700

    Firebase
    Added Firebase

--- Capstone Project/Capstone_ProjectApp.swift
+++ Capstone Project/Capstone_ProjectApp.swift
@@ -15,3 +15,31 @@ struct Capstone_ProjectApp: App {
         }
     }
 }
+
+//import SwiftUI
+//import FirebaseCore
+//
+//
+//class AppDelegate: NSObject, UIApplicationDelegate {
+//  func application(_ application: UIApplication,
+//                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+//    FirebaseApp.configure()
+//
+//    return true
+//  }
+//}
+//
+//@main
+//struct YourApp: App {
+//  // register app delegate for Firebase setup
+//  @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+//
+//
+//  var body: some Scene {
+//    WindowGroup {
+//      NavigationView {
+//        ContentView()
+//      }
+//    }
+//  }
+//}
