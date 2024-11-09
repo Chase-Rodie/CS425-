cfcf8b25989420d111a331da6f0d46fe91b6a665
Lexie Reddon <lreddon@unr.edu>
2024-11-09 09:34:30 -0800

    Small updates

--- WorkoutPlanner/WorkoutView.swift
+++ WorkoutPlanner/WorkoutView.swift
@@ -5,4 +5,24 @@
 //  Created by Chase Rodie on 10/31/24.
 //
 
+import SwiftUI
 import Foundation
+
+struct WorkoutView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "figure.highintensity.intervaltraining")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("WorkoutView Test")
+            Button(/*@START_MENU_TOKEN@*/"Button"/*@END_MENU_TOKEN@*/) {
+                /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/
+            }
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    WorkoutView()
+}
