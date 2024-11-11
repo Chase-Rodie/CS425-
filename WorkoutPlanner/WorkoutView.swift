4472c72e385820a24e92c538f0a64fc4627f2257
Lexie Reddon <lreddon@unr.edu>
2024-11-10 23:54:25 -0800

    building workoutview

--- WorkoutPlanner/WorkoutView.swift
+++ WorkoutPlanner/WorkoutView.swift
@@ -2,25 +2,123 @@
 //  WorkoutView.swift
 //  Fit Pantry
 //
-//  Created by Chase Rodie on 10/31/24.
-//
+//  Created by Lexie Reddon
+//  This will be the first view when the user selects "workout" on the navigation menu
 
 import SwiftUI
 import Foundation
 
 struct WorkoutView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "figure.highintensity.intervaltraining")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("WorkoutView Test")
-            Button(/*@START_MENU_TOKEN@*/"Button"/*@END_MENU_TOKEN@*/) {
-                print("Hello")
+        
+        
+//Horizontal stack for weekly list/view.
+        VStack{
+            Button("hello"){
+                print("test")
+            }
+
+            Text("Weekly Summary")
+            HStack(alignment: .top){
+                VStack{
+                    Image("workout")
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: 50)
+                    Text("Legs")
+                }
+                VStack{
+                    Image("workout")
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: 50)
+                    Text("Legs")
+                }
+                VStack{
+                    Image("workout")
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: 50)
+                    Text("Legs")
+                }
+                VStack{
+                    Image("workout")
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: 50)
+                    Text("Legs")
+                }
+                VStack{
+                    Image("workout")
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: 50)
+                    Text("Legs")
+                }
+                
             }
         }
         .padding()
-       
+    //Vertical stack for daily list/view.
+        
+        VStack(alignment: .leading){
+            Text("Today's Workout ")
+                .font(.title)
+            
+            Text("4 Exercises")
+            HStack {
+                
+                Image("workout")
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: 50)
+                VStack(alignment: .leading) {
+                    Text("Exercise 1")
+                    Text("4 Sets, 12 Reps, 25 Lbs")
+                }
+                Spacer()
+                    .frame(width: 50)
+                Image(systemName: "checkmark.circle.fill")
+                    .scaleEffect(2)
+                    .aspectRatio(contentMode: /*@START_MENU_TOKEN@*/.fill/*@END_MENU_TOKEN@*/)
+                    .foregroundColor(Color.green)
+                Spacer()
+                    .frame(width: 40)
+            }
+            HStack {
+                Image("workout")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 50)
+                VStack(alignment: .leading) {
+                    Text("Exercise 2")
+                    Text("4 Sets, 12 Reps, 25 Lbs")
+                }
+                Spacer()
+                    .frame(width: 50)
+                Image(systemName: "checkmark.circle.fill")
+                    .scaleEffect(2)
+                    .aspectRatio(contentMode: /*@START_MENU_TOKEN@*/.fill/*@END_MENU_TOKEN@*/)
+                    .foregroundColor(Color.green)
+                Spacer()
+                    .frame(width: 40)
+                
+            }
+            HStack {
+                Image("workout")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 50)
+                VStack(alignment: .leading) {
+                    Text("Exercise 3")
+                    Text("4 Sets, 12 Reps, 25 Lbs")
+                }
+                Spacer()
+                    .frame(width: 100)
+            }
+            
+        }
+        
     }
 }
 
