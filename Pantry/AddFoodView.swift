07ec5296d2f4e8f8e9f0c982121428edffe31c73
Greenhill_Zachary <zgreenhill@nevada.unr.edu>
2024-11-30 16:59:56 -0800

    Selectable Search Items
    Search results are displayed as a list of buttons that can be clicked. Currently just prints the document ID for each item.

--- Pantry/AddFoodView.swift
+++ Pantry/AddFoodView.swift
@@ -14,21 +14,47 @@ struct AddFoodView: View {
     
     var body: some View {
         VStack{
+            
             // Display search field
             TextField("Search", text: $searchText)
                 .padding()
                 .textFieldStyle(RoundedBorderTextFieldStyle())
             
                 // Update search on each character being entered
-                .onChange(of: searchText) { //newValue in
+                .onChange(of: searchText) {
                     searchManager.fetchItems(searchQuery: searchText)
                 }
+            
             // Display search results
             List(searchManager.items, id: \.id) {item in
-                Text(item.name)
+                //Text(item.name) //  Displays items as a list
+                
+                // Show search results as buttons
+                Button(action: {
+                    
+                    // Preform the function when item is pressed
+                    selectFood(item)
+                }) {
+                    
+                    // Display search results
+                    HStack {
+                        Text(item.name)
+                            .foregroundColor(.black)
+                    }
+                    .padding()
+                    .background(
+                        RoundedRectangle(cornerRadius: 8)
+                            .stroke(Color.blue))
+                }
             }
         }
         .padding()
+    }
+    
+    // Function for what happens when a food is selected
+    private func selectFood(_ item: Food) {
+        // Prompt user for quantity
+        print("Item tapped: \(item.id)")
     }
 }
 
