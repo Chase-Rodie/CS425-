bce25f3093c7825f3dd87a7a166d07988635a948
hamistani <123585710+hamistani@users.noreply.github.com>
2025-02-20 19:40:24 -0800

    Update ProfileView.swift

--- Core/Profile/ProfileView.swift
+++ Core/Profile/ProfileView.swift
@@ -3,18 +3,40 @@
 //  Fit Pantry
 //
 //  Created by Chase Rodie on 12/2/24.
+//  Edited by Heather Amistani on 02/18/2024
 //
 
 import SwiftUI
+import FirebaseAuth
+import FirebaseFirestore
+import FirebaseStorage
 
 @MainActor
 final class ProfileViewModel: ObservableObject {
     
     @Published private(set) var user: DatabaseUser? = nil
+    @Published var profileImage: UIImage? = nil
     
     func loadCurrentUser() async throws {
         let  authDataResult = try AuthenticationManager.shared.getAuthenticatedUser()
         self.user = try await UserManager.shared.getUser(userId: authDataResult.uid)
+        
+        if let profilePicURL = user?.profileImageUrl {
+            await loadProfileImage(from: profilePicURL)
+        }
+    }
+    
+    func loadProfileImage(from url: String) async{
+        guard let imageUrl = URL(string: url) else {return}
+        
+        do{
+            let data = try Data(contentsOf: imageUrl)
+            DispatchQueue.main.async{
+                self.profileImage = UIImage(data:data)
+            }
+        } catch{
+            print("Error loading image: \(error)")
+        }
     }
     
 }
@@ -25,31 +47,74 @@ struct ProfileView: View {
     @Binding var showSignInView: Bool
     
     var body: some View {
-        List {
-            if let user = viewModel.user {
-                Text("UserId: \(user.userId)")
-                
-                if let isAnonymous = user.isAnonymous {
-                    Text("Is Anonymous: \(isAnonymous.description.capitalized)")
+        NavigationView{
+            VStack{
+                //Profile Image
+                if let image = viewModel.profileImage{
+                    Image(uiImage: image)
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 100, height: 100)
+                        .clipShape(Circle())
+                        .padding()
+                } else{
+                    Image(systemName: "person.circle.fill")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 100, height: 100)
+                        .foregroundColor(.gray)
+                        .padding()
                 }
             }
         }
-        .task {
-            try? await viewModel.loadCurrentUser()
-        }
-        .navigationTitle("Profile")
-        .toolbar {
-            ToolbarItem(placement: .navigationBarTrailing) {
-                NavigationLink { SettingsView(showSignInView: $showSignInView)
-                } label: {
-                    Image(systemName: "gear")
-                        .font(.headline)
+        List {
+            if let user = viewModel.user {
+                Section(header: Text("Personal Info")){
+                    Text("Email: \(user.email ?? "N/A")")
+                    Text("Gender: \(user.gender ?? "N/A")")
+                    Text("Weight: \(user.weight ?? "N/A") lbs")
+                    Text("Height: \(user.height ?? "N/A") inches")
+                    Text("Allergies: \(user.allergies ?? "None")")
+                    Text("Exercise Level: \(user.exerciseLevel?.capitalized ?? "N/A")")
+                }
+                //                Text("UserId: \(user.userId)")
+                //
+                //                if let isAnonymous = user.isAnonymous {
+                //                    Text("Is Anonymous: \(isAnonymous.description.capitalized)")
+                //                }
+                //            }
+            }
+            Section{
+                NavigationLink(destination: EditProfileView(user: viewModel.user)){
+                    Text("EditProfile")
+                        .foregroundColor(.blue)
+                }
+            }
+            .task {
+                try? await viewModel.loadCurrentUser()
+            }
+            .navigationTitle("Profile")
+            .toolbar {
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    NavigationLink { SettingsView(showSignInView: $showSignInView)
+                    } label: {
+                        Image(systemName: "gear")
+                            .font(.headline)
+                    }
                 }
             }
         }
     }
 }
-
+    
+struct EditProfileView: View{
+    var user: DatabaseUser?
+    var body: some View{
+        Text("Edit Profile Screen")
+            .navigationTitle("Edit Profile")
+    }
+}
+    
 struct ProfileView_Previews: PreviewProvider {
     static var previews: some View {
         NavigationStack {
